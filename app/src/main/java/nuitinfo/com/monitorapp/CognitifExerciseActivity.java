90d347aacf7823016cb19d9fd380f3f9edf5ee71
bruhla <bruhla.info@gmail.com>
2018-12-06 21:33:04 +0100

    initial commit

---
+++ app/src/main/java/nuitinfo/com/monitorapp/CognitifExerciseActivity.java
@@ -0,0 +1,13 @@
+package nuitinfo.com.monitorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CognitifExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cognitif_exercise);
+    }
+}
